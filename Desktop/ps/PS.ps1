85ce8129a1da4461ebbaa443633a1bf02fd8b5ba
usejfijaj <usejfijaj2017@gmail.com>
2021-05-05 01:26:40 +0200

    remove folders

--- Desktop/ps/PS.ps1
+++
@@ -1,8 +0,0 @@
-Edit RAM usage in VM :
-Set-VMMemory TestVM -DynamicMemoryEnabled $true -MinimumBytes 64MB -StartupBytes 256MB -MaximumBytes 2GB -Priority 80 -Buffer 25
-
-See Windows Feature :
-Get-WindowsFeature | where {$_.installed -qe $true} | select DisplayName
-
-Join Computer in Domain:
-Add-Computer -ComputerName COMPUTER_NAMES_TO_BE_JOINED -DomainName DOMAIN.COM -Credential DOMAIN\USER -Restart
