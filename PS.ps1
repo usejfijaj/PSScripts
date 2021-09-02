4297ac126f6ab6b3d448672f64c1be93ccbb58d5
usejfijaj <usejfijaj2017@gmail.com>
2021-09-02 11:08:04 +0200

    Small update
    First commit

--- PS.ps1
+++ PS.ps1
@@ -4,5 +4,6 @@ Set-VMMemory TestVM -DynamicMemoryEnabled $true -MinimumBytes 64MB -StartupBytes
 See Windows Feature :
 Get-WindowsFeature | where {$_.installed -qe $true} | select DisplayName
 
+#Testing new commit from git
 Join Computer in Domain:
 Add-Computer -ComputerName COMPUTER_NAMES_TO_BE_JOINED -DomainName DOMAIN.COM -Credential DOMAIN\USER -Restart
